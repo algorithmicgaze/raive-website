744355b1afc025a2c6e4a1e761b3b9ed7c491a4a
Frederik De Bleser <frederik@debleser.be>
2024-04-18 20:34:42 +0200

    Initial version.

---
+++ main.js
@@ -0,0 +1,87 @@
+import * as twgl from "https://cdn.skypack.dev/twgl.js";
+import gsap from "https://cdn.skypack.dev/gsap";
+import ScrollTrigger from "https://cdn.skypack.dev/gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const gl = document.getElementById("c").getContext("webgl");
+const programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
+const arrays = {
+  position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
+};
+const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+
+const noiseTexture = twgl.createTexture(gl, { src: "img/noise.png" }, () =>
+  requestAnimationFrame(render)
+);
+const startTime = Date.now();
+
+const anims = {
+  light_dir_x: 0,
+  light_dir_y: -1,
+  light_dir_z: 0.1,
+  world_color_r: 1.0,
+  world_color_g: 0.1,
+  world_color_b: 0.1,
+};
+
+function render(time) {
+  twgl.resizeCanvasToDisplaySize(gl.canvas);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  const progress = tl.progress();
+
+  const uniforms = {
+    u_resolution: [gl.canvas.width, gl.canvas.height],
+    u_time: progress * 100.0,
+    u_world_color: [
+      anims.world_color_r,
+      anims.world_color_g,
+      anims.world_color_b,
+    ],
+    u_noise: noiseTexture,
+    u_light_dir: [anims.light_dir_x, anims.light_dir_y, anims.light_dir_z],
+  };
+  gl.useProgram(programInfo.program);
+  twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+  twgl.setUniforms(programInfo, uniforms);
+  twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLES);
+
+  requestAnimationFrame(render);
+}
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "main",
+    start: "top top",
+    end: "bottom bottom",
+    scrub: true,
+  },
+});
+
+tl.to(anims, {
+  light_dir_x: 0.0,
+  light_dir_y: -1.0,
+  light_dir_z: 0.1,
+  world_color_r: 0.8,
+  world_color_g: 0.1,
+  world_color_b: 0.1,
+  duration: 0.5,
+});
+tl.to(anims, {
+  light_dir_x: 0.0,
+  light_dir_y: 1.0,
+  light_dir_z: 0.1,
+  world_color_r: 0.8,
+  world_color_g: 0.1,
+  world_color_b: 0.1,
+  duration: 1,
+});
+tl.to(anims, {
+  light_dir_x: 0.0,
+  light_dir_y: 0.0,
+  light_dir_z: -0.1,
+  world_color_r: 0.4,
+  world_color_g: 0.4,
+  world_color_b: 0.4,
+  duration: 1,
+});
